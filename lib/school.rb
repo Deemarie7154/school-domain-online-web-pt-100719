4151162c1a88b9ef5b02c2589f11bd77cc46287f
Dwight Marie <dwightmarie@gmail.com>
2019-10-30 23:57:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School 
+  def initialize(name, roster = {})
+  @name = name
+  @roster = roster
+  end
+
+def name=(name) 
+  @name = name
+end
+
+def name 
+@name
+end
+
+def roster=(roster)
+@roster = roster
+end
+
+def roster
+  @roster 
+end
+
+def add_student(name, grade)
+  name = grade[name]
+end
+end
